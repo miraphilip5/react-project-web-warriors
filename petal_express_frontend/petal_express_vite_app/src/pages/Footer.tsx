6fdd44387a68ed8eda065e9e51ebe0db133f761a
miraphilip5 <miraphilip9@gmail.com>
2023-08-14 13:08:33 -0400

    footer style changes

--- petal_express_frontend/petal_express_vite_app/src/pages/Footer.tsx
+++ petal_express_frontend/petal_express_vite_app/src/pages/Footer.tsx
@@ -1,17 +1,90 @@
-import Box from '@mui/material/Box';
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: "#9c27b0",
+    color: "white",
+    textAlign: "center",
+    padding: "20px",
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    width: "100%",
+  };
 
-const footerStyle = {
-  backgroundColor: '#f5f5f5',
-  padding: '10px',
-  textAlign: 'center',
-};
+  const footerContentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
 
-function Footer() {
+  const iconStyle = {
+    fontSize: "2rem",
+    color: "white",
+    margin: "0 10px",
+    cursor: "pointer",
+    transition: "color 0.3s",
+  };
+
+  const copyrightStyle = {
+    fontSize: "0.9rem",
+    marginTop: "10px",
+  };
+
+  const ulStyle = {
+    display: "flex",
+    margin: "0",
+    padding: "0",
+    listStyleType: "none",
+  };
+
   return (
-    <Box sx={{ ...footerStyle, position: 'fixed', left: 0, bottom: 0, width: '100%', color: "#9c27b0" }}>
-      Web Warriors 2023
-    </Box>
+    <footer style={footerStyle}>
+      <div style={footerContentStyle}>
+        <div>
+          <ul style={ulStyle}>
+            <li>
+              <a href="https://www.facebook.com/">
+                <i
+                  style={iconStyle}
+                  className="fa fa-facebook"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/?hl=en">
+                <i
+                  style={iconStyle}
+                  className="fa fa-instagram"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.pinterest.com/">
+                <i
+                  style={iconStyle}
+                  className="fa fa-pinterest-p"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/?lang=en">
+                <i
+                  style={iconStyle}
+                  className="fa fa-twitter"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p style={copyrightStyle}>
+          &copy; 2023 Web Warriors. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
